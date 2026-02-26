9730569aaf73c899e171a9084fab8e1a2478cf2f
varunratnani-creator <varun.ratnani@somaiya.edu>
2026-02-26 18:19:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,8 @@
-import streamlit as st
-from ultralytics import YOLO
-import numpy as np
-from PIL import Image
+import cv2
 
-model = YOLO("best.pt")
+img_array = np.array(img)
 
-st.title("🚗 Number Plate Detection App")
-st.write("Upload an image to detect vehicle number plates.")
+# Resize image
+img_resized = cv2.resize(img_array, (640, 640))
 
-uploaded_file = st.file_uploader("Upload Image", type=["jpg", "png", "jpeg"])
-
-if uploaded_file is not None:
-    image = Image.open(uploaded_file)
-    img_array = np.array(image)
-
-    st.image(image, caption="Original Image", use_column_width=True)
-
-    results = model.predict(img_array, conf=0.25)
-
-    annotated_image = results[0].plot()
-
-    st.image(annotated_image, caption="Detected Image", use_column_width=True)
+results = model.predict(img_resized, conf=0.25)
